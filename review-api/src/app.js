8eeedf0219694baf9bb46f08c5bd8b45f3a9defd
Avishek <avishekmukherje007@gmail.com>
2025-11-07 03:21:18 +0530

    app.js ( not used )

--- review-api/src/app.js
+++ review-api/src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use('/api', reviewRoutes);
 
-app.use((err, req, res, next) => {
+app.use((err, res) => {
   logger.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
